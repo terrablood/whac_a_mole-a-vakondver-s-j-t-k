f397832c9f6dadd866354dd57a8fd6ddbc203a79
Levente Toth <leventet829@gmail.com>
2025-02-06 08:46:19 +0100

    huh

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-let divek = document.querySelectorAll('div') //az összes divet megkeresi!
+let divekListaja = document.querySelectorAll('main div') //az összes divet megkeresi!
 
 function keveres(t){ //megkeveri a beérkező lista elemeit
     let l = [...t] //átmásolja t elemeit l-be
-    for (let i =0; i < l.lenght; i++) {
-        let t = l[i];
-        let x = veletleegesz(i, l.lenght)
+    for (let i =0; i < l.length; i++) {
+        let temp = l[i];
+        let x = veletlenEgesz(i, l.length)
         l[i] = l[x];
-        l[x] = t;
+        l[x] = temp;
     }
     return l;
 }
 
-function veletleegesz(a,b){ //balról benne lehet, jobbról meg nem,  így a ketővel generál egy random számot
+function veletlenEgesz(a,b){ //balról benne lehet, jobbról meg nem,  így a ketővel generál egy random számot
     return a+Math.floor(Math.random()*(b-a));
 }
+
+function sorsolas(db){
+    
+    let l = keveres(divekListaja);
+    for (let i = 0; i < db; i++){
+        l[i].classList.toggle('piros');
+            
+    }
+    
+}
+function kiertekeles(e){
+    if(e.target.classList.contains('titkosüzenet')){
+        e.target.classList.toggle('piros');
+        e.target.classList.toggle('titkosüzenet');
+    }else{
+        alert('Nem ez volt az.')
+    }
+    
+}
+function mehetajatek(e){
+    for (const d of divekListaja) {
+        if(d.classList.contains('piros')){
+            d.classList.toggle('piros');
+            d.classList.toggle('titkosüzenet');
+        }
+    }
+}
+
+function main(){
+    sorsolas(5);
+    for (const e of divekListaja) {
+        e.addEventListener('click', kiertekeles);
+    }
+    mehet.addEventListener('click', mehetajatek)
+}
+main();
