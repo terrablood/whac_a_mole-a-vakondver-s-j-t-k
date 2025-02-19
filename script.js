e8e06f755e47c92cd64f928b56de779430a48e5d
Levente Toth <leventet829@gmail.com>
2025-02-19 13:54:09 +0100

    hzjjk

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let divekListaja = document.querySelectorAll('main div') //az összes divet megkeresi!
+let felforditottak = 0;
+let mik_vannak_felforditva =[];
 
 function keveres(t){ //megkeveri a beérkező lista elemeit
     let l = [...t] //átmásolja t elemeit l-be
@@ -10,40 +12,67 @@ function keveres(t){ //megkeveri a beérkező lista elemeit
     }
     return l;
 }
+function megvizsgal()
+{
+
+}
 
 function veletlenEgesz(a,b){ //balról benne lehet, jobbról meg nem,  így a ketővel generál egy random számot
     return a+Math.floor(Math.random()*(b-a));
 }
 
-function sorsolas(db){
+function sorsolas(){
     
     let l = keveres(divekListaja);
-    for (let i = 0; i < db; i++){
-        l[i].classList.toggle('piros');
-            
+    let s =0;
+    for (let i = 0; i < l.length; i++){
+        l[i].classList.toggle(s);
+        l[i+1].classList.toggle(s);
+        s++;
+        i++;
     }
     
 }
 function kiertekeles(e){
-    if(e.target.classList.contains('titkosüzenet')){
-        e.target.classList.toggle('piros');
-        e.target.classList.toggle('titkosüzenet');
-    }else{
-        alert('Nem ez volt az.')
-    }
     
-}
-function mehetajatek(e){
-    for (const d of divekListaja) {
-        if(d.classList.contains('piros')){
-            d.classList.toggle('piros');
-            d.classList.toggle('titkosüzenet');
+    if(felforditottak == 2){
+        alert('többet nem fordíthatsz fel!');
+    }
+    else
+    {
+        if(e.target.classList.contains('lefordított')){
+            e.target.classList.toggle('lefordított');
+            e.target.classList.toggle('felfordított');
+            felforditottak++;
+            mik_vannak_felforditva+=e.classList;
+        }else{
+            alert('ezt mar felfordítottad.')
         }
     }
 }
+function mehetajatek(e){
+    for (const d of divekListaja) {
+        if(d.classList.contains('felfordított')){
+            d.classList.toggle('felfordított');
+            d.classList.toggle('lefordított');
+        }
+    }
+    felforditottak = 0;
+}
 
 function main(){
-    sorsolas(5);
+   
+    for (const e of divekListaja) {
+        if(e.classList.contains('lefordított'))
+        {
+        
+        }
+        else
+        {
+            e.classList.toggle('lefordított');
+        }    
+    }
+    sorsolas();
     for (const e of divekListaja) {
         e.addEventListener('click', kiertekeles);
     }
